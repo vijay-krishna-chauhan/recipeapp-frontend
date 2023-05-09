81691cadbac09b3ac9d865e71e9a7d127b6ed3a3
Ritik Sachan <sachanritik1@gmail.com>
2023-05-10 00:55:45 +0530

    changed localhostapi to renderapi

--- src/Axios.js
+++ src/Axios.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from "axios"
 // axios.defaults.baseURL = "http://localhost:5000";
-axios.defaults.baseURL = "https://recipeappbackend4.onrender.com";
+axios.defaults.baseURL = "https://recipe-backend-jcxr.onrender.com/"
 axios.interceptors.request.use(
-    (config)=>{
-        const token=localStorage.getItem("token");
-        const auth=token ? `Bearer ${token}` : "";
-        if (config.headers) {
-            config.headers.Authorization = auth;
-          }
-        return config;
-    },
-    (error)=>Promise.reject(error)
-);
+	(config) => {
+		const token = localStorage.getItem("token")
+		const auth = token ? `Bearer ${token}` : ""
+		if (config.headers) {
+			config.headers.Authorization = auth
+		}
+		return config
+	},
+	(error) => Promise.reject(error)
+)
 
-export default axios;
+export default axios
